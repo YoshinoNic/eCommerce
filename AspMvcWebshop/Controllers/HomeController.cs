10c043ee7e7d4c9ad6ea31d4d4e1d260c3e78c93
YoshinoNic <yoshinonicusor@gmail.com>
2015-02-19 13:16:27 +0100

    eCommerce solution using MVC5
    eCommerce solution using MVC5

---
+++ AspMvcWebshop/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AspMvcWebshop.Models;
+
+namespace AspMvcWebshop.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        StoreEntities storeDB = new StoreEntities();
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            // Get most popular items
+            var products = GetTopSellingProducts(4);
+
+            return View(products);
+
+            //return "Hello from home";
+        }
+
+        //queries the DB for top selling products
+        private List<Product> GetTopSellingProducts(int count)
+        {
+            // Group the order details by album and return
+            // the albums with the highest count
+            return storeDB.Products
+                .OrderByDescending(a => a.OrderDetails.Count())
+                .Take(count)
+                .ToList();
+        }
+    }
+}
---
+++ AspMvcWebshop/Controllers/StoreController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AspMvcWebshop.Models;
+
+namespace AspMvcWebshop.Controllers
+{
+    public class StoreController : Controller
+    {
+        StoreEntities storeDB = new StoreEntities();
+
+        //
+        // GET: /Store/
+        public ActionResult Index()
+        {
+            var categories = storeDB.Categories.ToList();        
+            return View(categories);
+        }
+        //
+        // GET: /Store/Browse
+        //returns NULL?????
+        public ActionResult Browse(string category)
+        {
+            //prevent Javascript - HtmlEncode
+            var categoryModel = storeDB.Categories.Include("Products")
+                .SingleOrDefault(c => c.Name.Contains(category));
+                //lambda expression, we want a single object that it's
+                //name matches the defined value.                
+            
+            return View(categoryModel);
+        }
+
+        //
+        // GET: /Store/Details/5
+        public ActionResult Details(int id)
+        {
+            var product = storeDB.Products.Find(id);
+
+            return View(product);
+        }
+
+        //
+        // GET: /Store/GenreMenu
+        [ChildActionOnly]
+        public ActionResult CategoryMenu()
+        {
+            var genres = storeDB.Categories.ToList();
+            return PartialView(genres);
+        }
+
+        //partial views logic
+        [ChildActionOnly]
+        public ActionResult AdminMenu()
+        {
+            if (User.IsInRole("Administrator")) // TODO: Remove magic string
+            {
+                return PartialView("AdminMenu");
+            }
+            return new EmptyResult();
+        }
+
+        [ChildActionOnly]
+        public ActionResult GuestMenu()
+        {
+            if (!User.Identity.IsAuthenticated) // TODO: Remove magic string
+            {
+                return PartialView("GuestMenu");
+            }
+            return new EmptyResult();
+        }
+
+        [ChildActionOnly]
+        public ActionResult RegisteredUser()
+        {
+            if (User.Identity.IsAuthenticated && !User.IsInRole("Administrator")) // TODO: Remove magic string
+            {
+                return PartialView("RegisteredUserMenu");
+            }
+            return new EmptyResult();
+        }  
+    }
+}
